b350bf69ceb67dc87f905d2f2f17fae9ea75ad21
ianfc15 <ianfcampos5@gmail.com>
2023-12-03 21:25:16 -0300

    ian

---
+++ ProjetoIan.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#define quantidadeDEQuartos 30
+
+typedef struct {
+    char hospede[30];
+} hotel;
+
+void inicializarHotel(hotel nomedoHotel[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        nomedoHotel[i].hospede[0] = '\0';
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    hotel transilvania[30];
+    inicializarHotel(transilvania, 30);
+
+    int opcao;
+    char inserirMaisHospedes;
+    do {
+        printf("\n===== MENU =====\n");
+        printf("1. Inserir novo hóspede\n");
+        printf("2. Listar hóspede\n");
+        printf("3. Buscar hóspede\n");
+        printf("4. Editar hóspede\n");
+        printf("5. Remover hóspede\n");
+        printf("6. Mostrar quartos disponíveis\n");
+        printf("7. Salvar em arquivo lista de hóspede\n");
+        printf("0. Sair\n");
+        printf("Digite a opção desejada: ");
+        scanf("%d", &opcao);
+        system("cls");
+
+        switch (opcao) {
+            case 1:
+                // parte victor
+                break;
+            case 2:
+                // parte eduardo
+                break;
+            case 3:
+                // parte adenilson
+                break;
+            case 4:
+                // parte francisco
+                break;
+            case 5:
+                // parte kelvin
+                break;
+            case 6:
+                system("cls");
+                quartosDisponiveis(transilvania);
+                break;
+            case 7:
+                system("cls");
+                printf("opção ainda nao disponivel\n");
+                break;
+            case 0:
+                printf("Saindo do programa. Até logo!\n");
+                system("pause");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+
+    } while (opcao != 0);
+
+    return 0;
+}
