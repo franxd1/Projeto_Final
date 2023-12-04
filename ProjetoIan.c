1dc28c70af654ea5e1826b65d6ec5d01c7ec0d19
ianfc15 <ianfcampos5@gmail.com>
2023-12-03 22:17:36 -0300

    parte ian

--- ProjetoIan.c
+++ ProjetoIan.c
@@ -14,6 +14,101 @@ void inicializarHotel(hotel nomedoHotel[], int tamanho) {
     }
 }
 
+void inserirHospede(hotel nomedoHotel[], int numeroQuarto) {
+    if (nomedoHotel[numeroQuarto - 1].hospede[0] == '\0') {
+        printf("Digite o nome do hóspede: ");
+        getchar();
+        fgets(nomedoHotel[numeroQuarto - 1].hospede, sizeof(nomedoHotel[numeroQuarto - 1].hospede), stdin);
+        nomedoHotel[numeroQuarto - 1].hospede[strcspn(nomedoHotel[numeroQuarto - 1].hospede, "\n")] = '\0';
+        printf("Hóspede cadastrado com sucesso!\n");
+    } else {
+        printf("Quarto já ocupado. Escolha outro quarto.\n");
+    }
+}
+void listarHospedes(hotel nomedoHotel[]) {
+    printf("Lista de hóspede:\n");
+    for (int i = 0; i < 30; i++) {
+        if (nomedoHotel[i].hospede[0] != '\0') {
+            printf("Quarto %d: %s\n", i + 1, nomedoHotel[i].hospede);
+        }
+    }
+    system("pause");
+    system("cls");
+}
+void buscarHospede(hotel nomedoHotel[]) {
+    char nome[50];
+    printf("Digite o nome do hóspede a ser buscado: ");
+    getchar();
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+
+    for (int i = 0; i < quantidadeDEQuartos; i++) {
+        if (strcmp(nomedoHotel[i].hospede, nome) == 0) {
+            printf("%s está no quarto %d.\n", nome, i + 1);
+            return;
+        }
+    }
+
+    printf("%s não encontrado.\n", nome);
+    system("pause");
+    system("cls");
+}
+void editarHospede(hotel nomedoHotel[]) {
+    int numeroDoQuarto;
+    printf("Digite o número do quarto a ser editado: ");
+    scanf("%d", &numeroDoQuarto);
+
+    if (numeroDoQuarto >= 1 && numeroDoQuarto <= quantidadeDEQuartos) {
+        if (nomedoHotel[numeroDoQuarto - 1].hospede[0] != '\0') {
+            printf("Digite o novo nome do hóspede: ");
+            scanf("%s", nomedoHotel[numeroDoQuarto - 1].hospede);
+            printf("Hóspede editado com sucesso!\n");
+        } else {
+            printf("Quarto vazio. Nenhum hóspede para editar.\n");
+        }
+    } else {
+        printf("Número de quarto invúlido.\n");
+    }
+    system("pause");
+    system("cls");
+}
+void removerHospede(hotel nomedoHotel[]) {
+    int quarto;
+    printf("Digite o número do quarto a ser liberado: ");
+    scanf("%d", &quarto);
+
+    if (quarto >= 1 && quarto <= quantidadeDEQuartos) {
+        if (nomedoHotel[quarto - 1].hospede[0] != '\0') {
+            nomedoHotel[quarto - 1].hospede[0] = '\0';
+            printf("Hóspede removido com sucesso!\n");
+        } else {
+            printf("Quarto vazio. Nenhum hopede para remover.\n");
+        }
+    } else {
+        printf("Número de quarto inválido.\n");
+    }
+    system("pause");
+    system("cls");
+}
+void removerHospede(hotel nomedoHotel[]) {
+    int quarto;
+    printf("Digite o número do quarto a ser liberado: ");
+    scanf("%d", &quarto);
+
+    if (quarto >= 1 && quarto <= quantidadeDEQuartos) {
+        if (nomedoHotel[quarto - 1].hospede[0] != '\0') {
+            nomedoHotel[quarto - 1].hospede[0] = '\0';
+            printf("Hóspede removido com sucesso!\n");
+        } else {
+            printf("Quarto vazio. Nenhum hopede para remover.\n");
+        }
+    } else {
+        printf("Número de quarto inválido.\n");
+    }
+    system("pause");
+    system("cls");
+}
+
 int main() {
     setlocale(LC_ALL, "Portuguese");
     hotel transilvania[30];
@@ -24,12 +119,12 @@ int main() {
     do {
         printf("\n===== MENU =====\n");
         printf("1. Inserir novo hóspede\n");
-        printf("2. Listar hóspede\n");
+        printf("2. Listar hóspedes\n");
         printf("3. Buscar hóspede\n");
         printf("4. Editar hóspede\n");
         printf("5. Remover hóspede\n");
         printf("6. Mostrar quartos disponíveis\n");
-        printf("7. Salvar em arquivo lista de hóspede\n");
+        printf("7. Salvar em arquivo Lista de Hóspedes\n");
         printf("0. Sair\n");
         printf("Digite a opção desejada: ");
         scanf("%d", &opcao);
@@ -37,19 +132,33 @@ int main() {
 
         switch (opcao) {
             case 1:
-                // parte victor
+                do{
+                system("cls");
+                printf("Digite o número do quarto: ");
+                int numeroDoQuarto;
+                scanf(" %d", &numeroDoQuarto);
+                inserirHospede(transilvania, numeroDoQuarto);
+                printf("Deseja inserir mais Hóspedes? (S) para Sim e (N) para Não: ");
+                scanf(" %c", &inserirMaisHospedes);
+
+                }while(inserirMaisHospedes != 'N');
+                system("cls");
                 break;
             case 2:
-                // parte eduardo
+                system("cls");
+                listarHospedes(transilvania);
                 break;
             case 3:
-                // parte adenilson
+                system("cls");
+                buscarHospede(transilvania);
                 break;
             case 4:
-                // parte francisco
+                system("cls");
+                editarHospede(transilvania);
                 break;
             case 5:
-                // parte kelvin
+                system("cls");
+                removerHospede(transilvania);
                 break;
             case 6:
                 system("cls");
